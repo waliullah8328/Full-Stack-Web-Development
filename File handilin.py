59cc7c5c9378dc255c81d59bdfeffd5bbd39f687
Wali Ullah Ripon <69679369+waliullah8328@users.noreply.github.com>
2021-08-27 21:55:54 +0600

    Delete File handilin.py

--- File handilin.py
+++
@@ -1,8 +0,0 @@
-# File Handeling
-file = open("C:/Users/User/text.txt", "a")
-file.write("Welcome python class . It is very good time for me")
-file.write("Hi This Ripon")
-file.close()
-read_file = open("C:/Users/User/text.txt", "r")
-for i in read_file:
-    print(i)
